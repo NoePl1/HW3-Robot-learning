fd63ba8feb9cb2f79ff73263709a53ff7f4f0b04
Noé <noe.poulain01@gmail.com>
2025-03-17 21:48:08 +0000

    weird commit

--- hw8/roble/infrastructure/dqn_utils.py
+++ hw8/roble/infrastructure/dqn_utils.py
@@ -73,7 +73,7 @@ def get_env_kwargs(env_name):
             'learning_starts': 50000,
             'target_update_freq': 10000,
             'replay_buffer_size': 50000,
-            'num_timesteps': 500,
+            'num_timesteps': 750,
             'q_func': create_atari_q_network,
             'learning_freq': 4,
             'grad_norm_clipping': 10,
