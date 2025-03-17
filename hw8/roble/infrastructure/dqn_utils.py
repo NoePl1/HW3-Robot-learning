7658904008c3515a8953cf7a10286f93dcbaa4c5
Noé <noe.poulain01@gmail.com>
2025-03-17 16:26:41 +0000

    more runs

--- hw8/roble/infrastructure/dqn_utils.py
+++ hw8/roble/infrastructure/dqn_utils.py
@@ -92,7 +92,7 @@ def get_env_kwargs(env_name):
             'optimizer_spec': lander_optimizer(),
             'q_func': create_lander_q_network,
             'replay_buffer_size': 50000,
-            'batch_size': 32,
+            'batch_size': 128,
             'gamma': 1.00,
             'learning_starts': 1000,
             'learning_freq': 1,
@@ -100,7 +100,7 @@ def get_env_kwargs(env_name):
             'target_update_freq': 3000,
             'grad_norm_clipping': 10,
             'lander': True,
-            'num_timesteps': 1000,
+            'num_timesteps': 4500,
             'env_wrappers': lunar_empty_wrapper
         }
         kwargs['exploration_schedule'] = lander_exploration_schedule(kwargs['num_timesteps'])
